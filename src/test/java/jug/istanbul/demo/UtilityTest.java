6a2242980e620daa5c320b86e5b3dec70fe05b70
AltugAltintas <altuga@gmail.com>
2023-04-09 11:34:41 +0300

    tests added

--- src/test/java/jug/istanbul/demo/UtilityTest.java
+++ src/test/java/jug/istanbul/demo/UtilityTest.java
@@ -15,7 +15,21 @@ public class UtilityTest {
         // Counter counter = new Counter("dogs cats");
         Assert.assertEquals(2, count);
 
+    }
 
+    @Test
+    public void test2() {
+        int count = Counter.getResult("dog cat");
+        // Counter counter = new Counter("dogs cats");
+        Assert.assertEquals(0, count);
+
+    }
+
+    @Test
+    public void test3() {
+        int count = Counter.getResult("dog catr");
+        // Counter counter = new Counter("dogs cats");
+        Assert.assertEquals(0, count);
 
     }
 }
