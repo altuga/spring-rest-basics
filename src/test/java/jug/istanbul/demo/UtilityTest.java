0b6a5f9ae8bca75f85d30fb566b5c14fb6dd2686
AltugAltintas <altuga@gmail.com>
2023-04-12 11:20:14 +0300

    tests are added

--- src/test/java/jug/istanbul/demo/UtilityTest.java
+++ src/test/java/jug/istanbul/demo/UtilityTest.java
@@ -1,6 +1,15 @@
 package jug.istanbul.demo;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.of;
 
 import jug.istanbul.demo.utility.Counter;
 import org.junit.Assert;
@@ -13,7 +22,7 @@ public class UtilityTest {
     public void test() {
         int count = Counter.getResult("dogs cats");
         // Counter counter = new Counter("dogs cats");
-        Assert.assertEquals(2, count);
+        //Assert.assertEquals(2, count);
 
     }
 
@@ -21,7 +30,7 @@ public class UtilityTest {
     public void test2() {
         int count = Counter.getResult("dog cat");
         // Counter counter = new Counter("dogs cats");
-        Assert.assertEquals(0, count);
+        //Assert.assertEquals(0, count);
 
     }
 
@@ -29,7 +38,25 @@ public class UtilityTest {
     public void test3() {
         int count = Counter.getResult("dog catr");
         // Counter counter = new Counter("dogs cats");
-        Assert.assertEquals(0, count);
+        //Assert.assertEquals(1, count);
 
     }
+
+
+    @ParameterizedTest
+    @MethodSource("generator")
+    void test(String originalStr, Integer expected) {
+
+        Assert.assertEquals(Counter.getResult(originalStr), expected.intValue());
+    }
+
+    static Stream<Arguments> generator() {
+        return Stream.of(
+                of("dogs cats", 2),
+                of("dog cat", 0),
+                of("dog catr", 1),
+                of("1234", 0 )
+        );
+    }
+
 }
